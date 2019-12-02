0b319f2392951f48413aef14060957db37c986cb
singo3 <58432872+singo3@users.noreply.github.com>
2019-12-03 01:38:47 +0900

    dice gam
    easy dice game for python

---
+++ dice.py
@@ -0,0 +1,10 @@
+import random
+
+class Dice():
+    def __init__(self,val=6):
+        if val not in[4,6,8,12,20]:
+            raise Exception('そんな多面体はありません。')
+        self.face_num = val
+
+    def shoot(self):
+        return random.randint(1,self.face_num)
